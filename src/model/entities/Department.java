8d0c041f0ffc4eb4eae6a339d6a222c535054cbd
leojavadev <leojavadev@gmail.com>
2019-12-18 22:25:30 -0300

    Classe Department atualizada

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -1,9 +1,17 @@
-package entities;
+package model.entities;
 
-public class Department {
+import java.io.Serializable;
+
+public class Department implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
 	
 	private Integer id;
 	private String name;
+	
+	public Department() {
+		
+	}
 	
 	public Department(Integer id, String name) {
 		this.id = id;
@@ -31,7 +39,6 @@ public class Department {
 		final int prime = 31;
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		result = prime * result + ((name == null) ? 0 : name.hashCode());
 		return result;
 	}
 
@@ -48,11 +55,6 @@ public class Department {
 			if (other.id != null)
 				return false;
 		} else if (!id.equals(other.id))
-			return false;
-		if (name == null) {
-			if (other.name != null)
-				return false;
-		} else if (!name.equals(other.name))
 			return false;
 		return true;
 	}
